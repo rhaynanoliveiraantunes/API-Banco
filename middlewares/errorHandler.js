359333456ee679a6c191dc829b737b92ea0cb286
Rhaynana Oliveira Antunes <rhaynanoliveiraantunes@gmail.com>
2026-06-01 14:43:12 -0300

    updat

--- middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (error, req, res, next) => {
-  const statusCode = error.statusCode || 500;
-
-  res.status(statusCode).json({
-    error: error.message || "Erro interno do servidor",
-  });
-};
-
-export default errorHandler;
